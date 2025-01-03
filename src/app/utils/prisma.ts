aa275afe33f08273e98bb1062ad64980ac44a4ce
Pujon Das Auvi <auvipujondas@gmail.com>
2025-01-03 23:49:25 +0600

    prisma deploy error

--- src/app/utils/prisma.ts
+++ src/app/utils/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '../../../prisma/generated/client'
+
 import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient().$extends({
